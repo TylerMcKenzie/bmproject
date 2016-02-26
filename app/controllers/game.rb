6bd75b31e9bf4c6cf9dce519e8c8773cb85a3c8f
tylermckenzie <tyler.mckenzie30@aol.com>
2016-02-26 20:18:35 +0000

    Add finalish draft

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -5,3 +5,10 @@ get '/play' do
 		redirect '/'
 	end
 end
+
+post '/play' do 
+	data = JSON.parse(request.body.read)
+	now = DateTime.now
+	Game.create(user_id: current_user.id, survival_time: data["survived_time"].to_i, played_at: now)
+	erb :'game/_game_over', layout: false
+end
--- public/js/application.js
+++ public/js/application.js
@@ -2,32 +2,23 @@ $(document).ready(function() {
 	// STOP WATCH
 
 	var timeBox = $("#currentTime");
-	setInterval(updateClock);
-	var clock;
-	var offset;
+	var clock = setInterval(updateClock, 1000);
+	var clockTime = 0;
 	function updateClock(){
-		clock += diff(); 
+		clockTime += 1; 
 		render();
 	};
-	function diff(){
-		var now = Date.now();
-		var diff = now - offset;
-		offset = now;
-		return diff;
-	};
 	function render(){
-		timeBox.text(clock/1000);
+		timeBox.text(clockTime+"s");
 	};
 
 
 
 	// GAME DATA
-
  	var canvas = $("#gameCanvas").get(0).getContext("2d");
  	var canvasWidth = $("#gameCanvas").width();
  	var canvasHeight = $("#gameCanvas").height();
 	var FPS = 30;
-
 	var game = setInterval(function() {
 	  update();
 	  draw();
@@ -43,37 +34,66 @@ $(document).ready(function() {
 	function update(){
 		if(key===37){
 			if(player.x > 0){
-				player.x -= 8;		
+				player.x -= 10;		
+			}
+			if(player.x <= 10){
+				gameOver(game);
 			}
 		}
 		if(key===38){
 			if(player.y > 0){
 				player.y -= 5;
 			}
+			if(player.y <= 5){
+				gameOver(game);
+			}
 		}
 		if(key===39){
 			if(player.x < 285){
-				player.x += 8;
+				player.x += 10;
+			}
+			if(player.x >= 280){
+				gameOver(game);
 			}
 		}
 		if(key===40){
 			if(player.y < 145){
 				player.y += 5;
 			}
+			if(player.y >=140){
+				gameOver(game);
+			}
+
 		}
-		
 		var dX = player.x - enemy.x;
 		var dY = player.y - enemy.y;
+		
 		var distance = Math.sqrt((dX*dX)+(dY*dY));
 		if(distance>0){
-			enemy.x += (player.x-enemy.x)/18;
-			enemy.y += (player.y-enemy.y)/18;
+			enemy.x += (player.x-enemy.x)/enemy.speed;
+			enemy.y += (player.y-enemy.y)/enemy.speed;
 		}
-		if(distance<8.5){
-			alert("you died");
+		if(distance<8.25){
 			gameOver(game);
 		}
 		
+		if(clockTime === 10){
+
+			enemy.speed = 15;
+		}
+		if(clockTime === 20){
+			enemy.speed = 12;
+		}
+		if(clockTime === 30){
+			enemy.speed = 9;
+		}
+		if(clockTime === 45){
+			enemy.speed = 5;
+		}
+		if(clockTime === 60){
+			enemy.speed = 3;
+		}
+
 	};
 	function draw(){
 		canvas.clearRect(0,0, canvasWidth, canvasHeight);
@@ -81,10 +101,30 @@ $(document).ready(function() {
 		player.draw();
 		enemy.draw();
 	};
-	function gameOver(game){
-		clearInterval(game);
 
+	function gameOver(game){
+
+		saveResults(endResults());
+		clearInterval(game);
+		clearInterval(clock);
 	}
+
+	function saveResults(data){
+		var request = $.ajax({
+			method: "POST",
+			url: "/play",
+			data: data,
+			contentType: "application/json",
+		});
+		request.done(function(response){
+			$(".game").append(response);
+		});
+
+	};
+
+	function endResults() {
+		return JSON.stringify({survived_time: clockTime});
+	};
 	var player = {
 		color: "#00A",
 		x: 20,
@@ -107,16 +147,19 @@ $(document).ready(function() {
 		draw: function(){
 			canvas.fillStyle = this.color;
 			canvas.fillRect(this.x,this.y,this.width,this.height);
-		}
+		},
+		speed: 18
 
 	};
 
 	var board = {
+		x:10,
+		y:5,
+		bWidth: 280,
+		bHeight: 140,
 		draw: function(){
 			canvas.fillStyle = "#FFFFFF";
-			canvas.fillRect(10,5, 280, 140);
-		}
+			canvas.fillRect(this.x,this.y, this.bWidth, this.bHeight);
+		},
 	};
-
-
 });
